c6dd69db2d94d7ed8c0410b8a67373a588df359e
CowKeyMan <dancauchi1@gmail.com>
2022-06-11 15:53:55 +0300

    Code coverage now ignores test_main which is always static anyway

--- scripts/standalone/run_tests.sh
+++ scripts/standalone/run_tests.sh
@@ -4,6 +4,6 @@
 echo "\nRunning Tests:"
 ./build/src/test_main
 echo "\nRunning Lcov:"
-lcov --directory . --capture --output-file build/code_coverage.info --exclude "*/usr/**/*" --exclude "*_deps/**/*"
+lcov --directory . --capture --output-file build/code_coverage.info --exclude "*/usr/**/*" --exclude "*_deps/**/*" --exclude "*/test_main.cpp"
 echo "\nRunning genhtml:"
 genhtml build/code_coverage.info --output-directory ./docs/code_coverage/
