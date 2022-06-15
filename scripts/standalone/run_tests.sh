a0edabec3e0a5f4b4c215349389af0e8eaec8fff
CowKeyMan <dancauchi1@gmail.com>
2022-06-15 14:25:18 +0300

    code coverage ignores main

--- scripts/standalone/run_tests.sh
+++ scripts/standalone/run_tests.sh
@@ -5,6 +5,6 @@ echo "\nRunning Tests:"
 ./build/src/test_main_cuda
 ./build/src/test_main_cpu
 echo "\nRunning Lcov:"
-lcov --directory . --capture --output-file build/code_coverage.info --exclude "*/usr/**/*" --exclude "*_deps/**/*" --exclude "*/test_main.cpp" --exclude "/tmp/*" --exclude "*.cuh"
+lcov --directory . --capture --output-file build/code_coverage.info --exclude "*/usr/**/*" --exclude "*_deps/**/*" --exclude "*main.cpp" --exclude "/tmp/*" --exclude "*.cuh"
 echo "\nRunning genhtml:"
 genhtml build/code_coverage.info --output-directory ./docs/code_coverage/
