e1505d5f849fdaf4eefd4fdaaed092dcdf81c0c2
CowKeyMan <dancauchi1@gmail.com>
2022-06-15 17:58:43 +0300

    Change Utils formattings

--- src/Utils/BenchmarkUtils.h
+++ src/Utils/BenchmarkUtils.h
@@ -1,18 +1,15 @@
 #ifndef BENCHMARK_UTILS_H
 #define BENCHMARK_UTILS_H
 
-#ifndef NDEBUG
-#include <chrono>
-#include <iostream>
-#endif
-
 /**
  * @file BenchmarkUtils.h
  * @brief A collection of utility scripts used for benchmarking
  * */
 
+#include <chrono>
+#include <iostream>
+
 // Macro for timing objects. The result is stored in TIME_IT_TOTAL
-#ifndef NDEBUG
 std::chrono::system_clock::time_point TIME_IT_START_TIME, TIME_IT_END_TIME;
 unsigned long long TIME_IT_TOTAL;
 #define TIME_IT(code_block)                                              \
@@ -20,11 +17,8 @@ unsigned long long TIME_IT_TOTAL;
   code_block;                                                            \
   TIME_IT_END_TIME = std::chrono::high_resolution_clock::now();          \
   TIME_IT_TOTAL = std::chrono::duration_cast<std::chrono::milliseconds>( \
-                  TIME_IT_END_TIME - TIME_IT_START_TIME                  \
+                    TIME_IT_END_TIME - TIME_IT_START_TIME                \
   )                                                                      \
-                  .count();
-#else
-#define TIME_IT(code_block) code_block;
-#endif
+                    .count();
 
 #endif
