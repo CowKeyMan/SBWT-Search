50214a80190a66675373a05349a3817b2a718181
CowKeyMan <dancauchi1@gmail.com>
2022-06-18 01:25:37 +0300

    Remove unneccessary target in build_docs

--- scripts/build/build_docs.sh
+++ scripts/build/build_docs.sh
@@ -3,6 +3,7 @@
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CLANG_TIDY=OFF -DENABLE_HEADER_GUARDS_CHECK=OFF -DENABLE_CLANG_FORMAT_CHECK=OFF -DBUILD_CPU=OFF -DBUILD_CUDA=OFF -DBUILD_MAIN=OFF -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF .. # to build nothing
+cmake --build . -j8
 cd ../documentation
 rm `grep -r -L -P "(.gitignore)" RST`
 doxygen Doxyfile
