e73e03284c41c3be30b0e58d94b261eb84442402
CowKeyMan <dancauchi1@gmail.com>
2022-06-18 01:46:48 +0300

    Update benchmark sbatch script

--- scripts/sbatch/run_benchmarks.sh
+++ scripts/sbatch/run_benchmarks.sh
@@ -1,10 +1,45 @@
-!#/bin/bash
+!/bin/bash
 #SBATCH --job-name=BenchmarkQueryReader
-#SBATCH --account=<project>
+
+## The account to charge. Look at the unix group from https://my.csc.fi/myProjects
+#SBATCH --account=${PROJECT}
+
+## Maximum time
 #SBATCH --time=02:00:00
 
-#SBATCH --mem-per-cpu=2G
-#SBATCH --partition=small
-##SBATCH --mail-type=BEGIN #uncomment to enable mail
+## put stderr to err.txt
+#SBATCH --error err.txt
 
+## put stdout to out.txt
+#SBATCH --output out.txt
+
+## We only need a single node
+#SBATCH --nodes 1
+
+## Number of cores per node. We request 8 for faster compilation
+#SBATCH --ntasks-per-node=8
+
+## Since this is a benchmarking script, we do not want it to share resources
+##SBATCH --exclusive
+
+## Amount of memory per node
+#SBATCH --mem-per-cpu=2G
+
+## This partition on mahti has NVME
+#SBATCH --partition=gpusmall
+
+## We only want a single a100 gpu and 4GB of NVME storage
+#SBATCH --gres=gpu:a100:1,nvme:4
+
+## Remove unnecessary modules
 module purge
+
+## Load in-build modules
+module load cmake
+module load gcc
+module load cuda
+module load python-data
+module load bzip2
+
+sh scripts/build/build_benchmarks.sh
+sh scripts/standalone/run_benchmarks.sh
