5c2a18b063dbfec5950c8c68da942741300298cc
CowKeyMan <dancauchi1@gmail.com>
2022-06-17 09:34:16 +0300

    Implement QueryReader into the main

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,16 @@
 #include <string>
 
 #include "ArgumentParser.hpp"
+#include "QueryReader.h"
 
 using sbwt_search::parse_arguments;
+using sbwt_search::QueryReader;
+using std::string;
 
-auto main(int argc, char **argv) -> int { parse_arguments(argc, argv); }
+const auto kmer_size = 30;
+
+auto main(int argc, char **argv) -> int {
+  auto args = parse_arguments(argc, argv);
+  auto query_reader = QueryReader(args["q"].as<string>(), kmer_size);
+  query_reader.parse_kseqpp_streams();
+}
