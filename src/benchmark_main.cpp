540d2dac0f8074ece023f21453930fb29a8e7496
CowKeyMan <dancauchi1@gmail.com>
2022-06-20 19:34:23 +0300

    Add gzip benchmark

--- src/benchmark_main.cpp
+++ src/benchmark_main.cpp
@@ -13,42 +13,27 @@ auto QueryReader_parse_kseqpp_gz_stream() -> void;
 auto QueryReader_parse_kseqpp_gz_read() -> void;
 
 const auto fna_filename = "benchmark_objects/GRCh38_latest_genomic.fna";
+const auto fna_zipped = "benchmark_objects/GRCh38_latest_genomic.fna.gz";
 
 int main() {
 #ifndef BENCHMARK_QUERY_READER
 #define BENCHMARK_QUERY_READER
+  TIME_IT({QueryReader(fna_filename, 30).parse_kseqpp_streams();})
+  cout << "QueryReader_kseqpp_streams: " << TIME_IT_TOTAL << '\n';
+  TIME_IT({QueryReader(fna_filename, 30).parse_kseqpp_read();})
+  cout << "QueryReader_kseqpp_read: " << TIME_IT_TOTAL << '\n';
+  TIME_IT({QueryReader(fna_filename, 30).parse_kseqpp_gz_stream();})
+  cout << "QueryReader_kseqpp_gz_stream: " << TIME_IT_TOTAL << '\n';
+  TIME_IT({QueryReader(fna_filename, 30).parse_kseqpp_gz_read();})
+  cout << "QueryReader_kseqpp_gz_read: " << TIME_IT_TOTAL << '\n';
 
-  TIME_IT(QueryReader_parse_kseqpp_streams());
-  cout << "QueryReader_parse_kseqpp_streams: " << TIME_IT_TOTAL << '\n';
-
-  TIME_IT(QueryReader_parse_kseqpp_read());
-  cout << "QueryReader_parse_kseqpp_read: " << TIME_IT_TOTAL << '\n';
-
-  TIME_IT(QueryReader_parse_kseqpp_gz_stream());
-  cout << "QueryReader_parse_kseqpp_gz_stream: " << TIME_IT_TOTAL << '\n';
-
-  TIME_IT(QueryReader_parse_kseqpp_gz_read());
-  cout << "QueryReader_parse_kseqpp_gz_read: " << TIME_IT_TOTAL << '\n';
-
+  TIME_IT({QueryReader(fna_zipped, 30).parse_kseqpp_streams();})
+  cout << "QueryReader_kseqpp_streams_zip: " << TIME_IT_TOTAL << '\n';
+  TIME_IT({QueryReader(fna_zipped, 30).parse_kseqpp_read();})
+  cout << "QueryReader_kseqpp_read_zip: " << TIME_IT_TOTAL << '\n';
+  TIME_IT({QueryReader(fna_zipped, 30).parse_kseqpp_gz_stream();})
+  cout << "QueryReader_kseqpp_gz_stream_zip: " << TIME_IT_TOTAL << '\n';
+  TIME_IT({QueryReader(fna_zipped, 30).parse_kseqpp_gz_read();})
+  cout << "QueryReader_kseqpp_gz_read_zip: " << TIME_IT_TOTAL << '\n';
 #endif
-}
-
-auto QueryReader_parse_kseqpp_streams() -> void {
-  auto query_reader = QueryReader(fna_filename, 30);
-  query_reader.parse_kseqpp_streams();
-}
-
-auto QueryReader_parse_kseqpp_read() -> void {
-  auto query_reader = QueryReader(fna_filename, 30);
-  query_reader.parse_kseqpp_read();
-}
-
-auto QueryReader_parse_kseqpp_gz_stream() -> void {
-  auto query_reader = QueryReader(fna_filename, 30);
-  query_reader.parse_kseqpp_gz_stream();
-}
-
-auto QueryReader_parse_kseqpp_gz_read() -> void {
-  auto query_reader = QueryReader(fna_filename, 30);
-  query_reader.parse_kseqpp_gz_read();
 }
