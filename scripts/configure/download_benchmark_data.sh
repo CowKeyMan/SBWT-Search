3656bc026503bef291fb37d2a595b938beb1c5af
CowKeyMan <dancauchi1@gmail.com>
2022-06-21 12:01:15 +0300

    Add source of fasta dataset

--- scripts/configure/download_benchmark_data.sh
+++ scripts/configure/download_benchmark_data.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 mkdir -p benchmark_objects
 cd benchmark_objects
 
 GRCh38="GRCh38_latest_genomic.fna"
+# Source: https://www.ncbi.nlm.nih.gov/projects/genome/guide/human/index.shtml
 wget -nc https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh38_latest/refseq_identifiers/${GRCh38}.gz
 if test -f "${GRCh38}"; then
         echo "${GRCh38} already unzipped, skipping "
