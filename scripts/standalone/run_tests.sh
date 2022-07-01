2a541597e2816bee278e1439c12dc3e74038e2fa
CowKeyMan <dancauchi1@gmail.com>
2022-07-01 15:16:05 +0300

    Update run_tests script to be less unnecessarily verbose

--- scripts/standalone/run_tests.sh
+++ scripts/standalone/run_tests.sh
@@ -1,10 +1,11 @@
 # Run the tests as well as generate code coverage
 
 #!/bin/sh
-echo "\nRunning Tests:"
+printf "\nRunning Tests:"
 ./build/bin/test_main_cuda
 ./build/bin/test_main_cpu
-echo "\nRunning Lcov:"
-lcov --directory . --capture --output-file build/code_coverage.info --exclude "*/usr/**/*" --exclude "*_deps/**/*" --exclude "*main.cpp" --exclude "/tmp/*" --exclude "*.cuh" --exclude "*/external/*"
-echo "\nRunning genhtml:"
-genhtml build/code_coverage.info --output-directory ./docs/code_coverage/
+printf "\nRunning Lcov..."
+lcov --directory . --capture -q --output-file build/code_coverage.info --exclude "*/usr/**/*" --exclude "*_deps/**/*" --exclude "*main.cpp" --exclude "/tmp/*" --exclude "*.cuh" --exclude "*/external/*"
+printf "\nRunning genhtml..."
+genhtml -q build/code_coverage.info --output-directory ./docs/code_coverage/
+printf "\n"
