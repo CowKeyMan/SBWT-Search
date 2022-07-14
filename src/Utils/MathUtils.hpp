017d307e505d0f5ffe2b61e85c349b8d61fd1365
CowKeyMan <dancauchi1@gmail.com>
2022-07-14 16:34:46 +0300

    Add divisible_by_power_of_two in math utils

--- src/Utils/MathUtils.hpp
+++ src/Utils/MathUtils.hpp
@@ -11,13 +11,24 @@
 using std::is_unsigned;
 
 template <class UnsignedNumber>
-UnsignedNumber
-round_up(const UnsignedNumber value, const UnsignedNumber multiple) {
+auto round_up(const UnsignedNumber value, const UnsignedNumber multiple)
+  -> UnsignedNumber {
   static_assert(
     is_unsigned<UnsignedNumber>::value,
     "Template class to round_up must be unsigned"
   );
   return (value + multiple - 1) / multiple * multiple;
+}
+
+template <class UnsignedNumber>
+inline auto divisible_by_power_of_two(
+  const UnsignedNumber numerator, const UnsignedNumber denominator_power_of_two
+) -> UnsignedNumber {
+  static_assert(
+    is_unsigned<UnsignedNumber>::value,
+    "Template class to round_up must be unsigned"
+  );
+  return (numerator & (denominator_power_of_two - 1)) == 0;
 }
 
 #endif
--- src/Utils/MathUtils_test.cpp
+++ src/Utils/MathUtils_test.cpp
@@ -10,3 +10,9 @@ TEST(MathUtils, RoundUp) {
   ASSERT_EQ(128, round_up<u64>(65, 64));
   ASSERT_EQ(64, round_up<u64>(64, 64));
 }
+
+TEST(MathUtils, DivisibleByPowerOfTwo) {
+  ASSERT_EQ(divisible_by_power_of_two<u64>(32, 4), true);
+  ASSERT_EQ(divisible_by_power_of_two<u64>(1024, 64), true);
+  ASSERT_EQ(divisible_by_power_of_two<u64>(64, 1024), false);
+}
