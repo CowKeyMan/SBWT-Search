c677ae169c5a5e369288d49ac73565f6a2c2fb07
CowKeyMan <dancauchi1@gmail.com>
2022-07-14 15:32:26 +0300

    Be more verbose when running CUDA vs CPU tests

--- scripts/standalone/run_tests.sh
+++ scripts/standalone/run_tests.sh
@@ -1,8 +1,9 @@
 # Run the tests as well as generate code coverage
 
 #!/bin/sh
-printf "\nRunning Tests:"
+printf "\nRunning CUDA Tests:\n"
 ./build/bin/test_main_cuda
+printf "\nRunning CPU Tests:\n"
 ./build/bin/test_main_cpu
 printf "\nRunning Lcov..."
 lcov --directory . --capture -q --output-file build/code_coverage.info --exclude "*/usr/**/*" --exclude "*_deps/**/*" --exclude "*main.cpp" --exclude "/tmp/*" --exclude "*.cuh" --exclude "*/external/*"
