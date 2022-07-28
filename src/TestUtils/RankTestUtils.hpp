00dd3ad62ccbd0f66d6f070d315c1fac5773d856
CowKeyMan <dancauchi1@gmail.com>
2022-07-28 17:57:17 +0300

    Stop using undefined behavious in rank utils

--- src/TestUtils/RankTestUtils.hpp
+++ src/TestUtils/RankTestUtils.hpp
@@ -12,19 +12,17 @@ namespace sbwt_search {
 
 template <bool reversed_bits>
 u64 dummy_cpu_rank(const u64 *v, const u64 index) {
-  const u64 final_index = (index - 1) / 64;
+  const u64 final_index = (index) / 64;
   u64 result = 0;
   for (int i = 0; i < final_index; ++i) {
     result += __builtin_popcountll(v[i]);
   }
   const u64 final_int = v[final_index];
   u64 required_bits;
-  // note: if we remove %64 here, tests pass as well. Not sure why, requires
-  // investigation in case it is an optimisation
   if (reversed_bits) {
-    required_bits = (final_int << (64ULL - (index % 64)));
+    required_bits = (final_int << (64 - (index % 64))) & (-((index % 64) != 0));
   } else {
-    required_bits = (final_int >> (64ULL - (index % 64)));
+    required_bits = (final_int >> (64 - (index % 64))) & (-((index % 64) != 0));
   }
   result += __builtin_popcountll(required_bits);
   return result;
