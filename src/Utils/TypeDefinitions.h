8dc6a4fcf275ab15676c38e887e41de92d3ce18a
CowKeyMan <dancauchi1@gmail.com>
2022-07-28 17:45:13 +0300

    Add uint8 to TypeDefinitions

--- src/Utils/TypeDefinitions.h
+++ src/Utils/TypeDefinitions.h
@@ -14,10 +14,12 @@ using std::int32_t;
 using std::int64_t;
 using std::uint32_t;
 using std::uint64_t;
+using std::uint8_t;
 
 using uint = unsigned int;
 using u64 = uint64_t;
 using u32 = uint32_t;
+using u8 = uint8_t;
 
 using s64 = int64_t;
 using s32 = int32_t;
