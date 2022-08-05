e9bbc1c7d21f2400f8a41ca38813a9fb74c10023
CowKeyMan <dancauchi1@gmail.com>
2022-08-05 20:05:46 +0300

    Replase BoundedSemaphore implementation with a more efficient version

--- src/Utils/BoundedSemaphore.hpp
+++ src/Utils/BoundedSemaphore.hpp
@@ -7,11 +7,7 @@
  * */
 
 #include <climits>
-
-#include "Semaphore.hpp"
-
-#include <iostream>
-using namespace std;
+#include <omp.h>
 
 namespace threading_utils {
 
@@ -19,20 +15,50 @@ using uint = unsigned int;
 
 class BoundedSemaphore {
   private:
-    Semaphore sem_main, sem_support;
+    omp_lock_t count_protector_, acquire_gate_, release_gate_;
+    omp_lock_t *count_protector = &count_protector_,
+               *acquire_gate = &acquire_gate_, *release_gate = &release_gate_;
 
   public:
-    BoundedSemaphore(uint starting_count, uint maximum_count=UINT_MAX):
-        sem_main(starting_count), sem_support(maximum_count - starting_count) {}
+    uint count, maximum;
 
-    void release() {
-      sem_support.acquire();
-      sem_main.release();
+  public:
+    BoundedSemaphore(uint starting_count=1, uint maximum=UINT_MAX):
+        count(starting_count), maximum(maximum) {
+      omp_init_lock(count_protector);
+      omp_init_lock(acquire_gate);
+      omp_init_lock(release_gate);
+      if (starting_count == 0) {
+        omp_set_lock(acquire_gate);
+      } else if (starting_count == maximum) {
+        omp_set_lock(release_gate);
+      }
     }
 
     void acquire() {
-      sem_main.acquire();
-      sem_support.release();
+      omp_set_lock(acquire_gate);
+      omp_set_lock(count_protector);
+      uint previous_count = count;
+      --count;
+      if (count > 0) { omp_unset_lock(acquire_gate); }
+      if (previous_count == maximum) { omp_unset_lock(release_gate); }
+      omp_unset_lock(count_protector);
+    }
+
+    void release() {
+      omp_set_lock(release_gate);
+      omp_set_lock(count_protector);
+      uint previous_count = count;
+      ++count;
+      if (previous_count == 0) { omp_unset_lock(acquire_gate); }
+      if (count < maximum) { omp_unset_lock(release_gate); }
+      omp_unset_lock(count_protector);
+    }
+
+    ~BoundedSemaphore() {
+      omp_destroy_lock(count_protector);
+      omp_destroy_lock(acquire_gate);
+      omp_destroy_lock(release_gate);
     }
 };
 
