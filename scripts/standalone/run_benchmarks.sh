ac837a8edc1615e262355af6132ea60b4e0ad760
CowKeyMan <dancauchi1@gmail.com>
2022-09-01 10:50:21 +0300

    Adjust benchmarks sbatch script to repeat the main file repeatedla nd report its results via logs

--- scripts/standalone/run_benchmarks.sh
+++ scripts/standalone/run_benchmarks.sh
@@ -1,42 +1,25 @@
 # Run the benchmark_main several times and save the results to a file
 
 
-mkdir -p benchmark_results
-mkdir -p tmp
-
-DATETIME="$(date +"%Y-%m-%d_%I-%M-%S_%p")"
-OUTPUT_FILE_NAME="benchmark_results/${DATETIME}_benchmark.txt"
-TEMP_FILE_NAME="tmp/current_benchmark.txt"
-BENCHMARK_AVERAGES_FILE_NAME="benchmark_results/averages.txt"
-
-# Add or comment arguments from here
-ARGUMENTS_LIST=(
-  "-SEQUENCE_FILE_PARSER_FASTA"
-  "-SEQUENCE_FILE_PARSER_FASTA_ZIP"
-  "-SEQUENCE_FILE_PARSER_FASTQ"
-  "-SEQUENCE_FILE_PARSER_FASTQ_ZIP"
-  "-RAW_SEQUENCE_PARSER_CHAR_TO_BITS"
+mkdir -p results
+input_files=(
+  "benchmark_objects/FASTA1GB.fna"
+  "benchmark_objects/FASTQ1GB.fnq"
+  "test_objects/365.fna"
+  "combined.txt"
 )
-ARGUMENTS=""
-for ARGUMENT in ${ARGUMENTS_LIST[@]};
-do
-  ARGUMENTS+="${ARGUMENT} "
+output_files=(
+  "results/FASTA1GB.txt"
+  "results/FASTQ1GB.txt"
+  "results/365.txt"
+  "results/combined_output.txt"
+)
+sbwt_file="test_objects/parsed100.sbwt"
+batches=(1, 3, 10, 30, 70, 100, 200, 1000)
+
+for batch in ${batches[@]}; do
+for (( i=0; i<${#input_files[@]}; i++ )); do
+echo Now running file ${input_file[i]} with ${batch} batches
+./build/bin/main_cuda -i ${sbwt_file} -q ${input_files[i]} -o ${output_files[i]} -b ${batch} -u 429496729600
 done
-
-
-# Run once so that we do not get any weird items taking longer than they should
-build/bin/benchmark_main ${ARGUMENTS} > /dev/null 2>/dev/null
-echo "Finished dummy benchmark run"
-for i in {1..4}
-do
-  build/bin/benchmark_main ${ARGUMENTS} >> "${TEMP_FILE_NAME}" 2>/dev/null
-  printf '\n' >> "${TEMP_FILE_NAME}"
-  echo "Finished benchmark run ${i}"
 done
-
-cat "${TEMP_FILE_NAME}" > "${OUTPUT_FILE_NAME}"
-rm "${TEMP_FILE_NAME}"
-
-echo "${DATETIME}" >> "${BENCHMARK_AVERAGES_FILE_NAME}"
-python3 scripts/standalone/print_benchmark_averages.py "${OUTPUT_FILE_NAME}" >> "${BENCHMARK_AVERAGES_FILE_NAME}"
-printf '\n' >> "${BENCHMARK_AVERAGES_FILE_NAME}"
