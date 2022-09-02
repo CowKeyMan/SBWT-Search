d984b022671121813eed24e3f508d4b4dc1a3939
CowKeyMan <dancauchi1@gmail.com>
2022-09-02 16:56:09 +0300

    Benchmarking script now also builds the combined input and output files

--- scripts/standalone/run_benchmarks.sh
+++ scripts/standalone/run_benchmarks.sh
@@ -1,25 +1,47 @@
 # Run the benchmark_main several times and save the results to a file
 
+DATETIME="$(date +"%Y-%m-%d_%I-%M-%S_%p")"
+
+## By default this is 1, which would halt our program
+unset OMP_NUM_THREADS
+## adjust this accordingly - by default it is warn
+export SPDLOG_LEVEL=TRACE
+
+results_folder="results/${DATETIME}"
 
 mkdir -p results
+mkdir -p ${results_folder}
+
+
 input_files=(
   "benchmark_objects/FASTA1GB.fna"
   "benchmark_objects/FASTQ1GB.fnq"
   "test_objects/365.fna"
-  "combined.txt"
+  "benchmark_objects/combined_input.txt"
 )
+
+# populate combined_input.txt
+printf "" > "benchmark_objects/combined_input.txt"
+printf "" > "benchmark_objects/combined_output.txt"
+for (( i=0; i<${#input_files[@]}-1; i++ )); do
+printf "${input_files[i]}\n" >> "benchmark_objects/combined_input.txt"
+printf "file_${i}\n" >> "benchmark_objects/combined_output.txt"
+done
+
 output_files=(
-  "results/FASTA1GB.txt"
-  "results/FASTQ1GB.txt"
-  "results/365.txt"
-  "results/combined_output.txt"
+  "${results_folder}/FASTA1GB.txt"
+  "${results_folder}/FASTQ1GB.txt"
+  "${results_folder}/365.txt"
+  "benchmark_objects/combined_output.txt"
 )
-sbwt_file="test_objects/parsed100.sbwt"
-batches=(1, 3, 10, 30, 70, 100, 200, 1000)
+sbwt_file="data/coli3.sbwt"
+batches=(1 3 10 30 70 100 200 1000)
+
+stdoutput="${results_folder}/benchmark_out.txt"
 
 for batch in ${batches[@]}; do
 for (( i=0; i<${#input_files[@]}; i++ )); do
-echo Now running file ${input_file[i]} with ${batch} batches
-./build/bin/main_cuda -i ${sbwt_file} -q ${input_files[i]} -o ${output_files[i]} -b ${batch} -u 429496729600
+echo Now running: File ${input_files[i]} with ${batch} batches >> ${stdoutput}
+./build/bin/main_cuda -i ${sbwt_file} -q ${input_files[i]} -o ${output_files[i]} -b ${batch} >> ${stdoutput}
 done
 done
