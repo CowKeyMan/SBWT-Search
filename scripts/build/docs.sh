66d5ca27f71fa10ad1ea899538f7c4dbe7064bbd
CowKeyMan <dancauchi1@gmail.com>
2023-01-30 15:04:52 +0200

    Fix documentation bug and remove static analysers from cmake

--- scripts/build/docs.sh
+++ scripts/build/docs.sh
@@ -18,9 +18,6 @@ then
   cmake \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=OFF \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DENABLE_CLANG_TIDY=OFF \
-    -DENABLE_HEADER_GUARDS_CHECK=OFF \
-    -DENABLE_CLANG_FORMAT_CHECK=OFF \
     -DBUILD_VERIFY=OFF \
     -DBUILD_MAIN=OFF \
     -DBUILD_TESTS=OFF \
@@ -31,7 +28,7 @@ then
 fi
 cmake --build . -j8
 cd ../documentation
-rm `grep -r -L -P "(.gitignore)" RST`
+rm `grep -r -L -P "(.gitignore|.dir_docstring)" RST`
 doxygen Doxyfile
 make html
 cd ..
