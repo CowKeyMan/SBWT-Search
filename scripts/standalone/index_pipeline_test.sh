1d75a9e982ab20b04e65b88dfae26691247e15e7
CowKeyMan <dancauchi1@gmail.com>
2023-02-23 15:37:36 +0200

    Rename combined*.txt to combined*.lst and remove actual after index pipeeline test

--- scripts/standalone/index_pipeline_test.sh
+++ scripts/standalone/index_pipeline_test.sh
@@ -7,9 +7,9 @@
 # build
 ./scripts/build/release.sh
 # run
-./build/bin/sbwt_search index -o test_objects/full_pipeline/index_search/combined_output.txt -i test_objects/search_test_index.sbwt -q test_objects/full_pipeline/index_search/combined_input.txt -b 5 -c ascii
-./build/bin/sbwt_search index -o test_objects/full_pipeline/index_search/combined_output.txt -i test_objects/search_test_index.sbwt -q test_objects/full_pipeline/index_search/combined_input.txt -b 5 -c binary
-./build/bin/sbwt_search index -o test_objects/full_pipeline/index_search/combined_output.txt -i test_objects/search_test_index.sbwt -q test_objects/full_pipeline/index_search/combined_input.txt -b 5 -c bool
+./build/bin/sbwt_search index -o test_objects/full_pipeline/index_search/combined_output.list -i test_objects/search_test_index.sbwt -q test_objects/full_pipeline/index_search/combined_input.list -b 5 -c ascii
+./build/bin/sbwt_search index -o test_objects/full_pipeline/index_search/combined_output.list -i test_objects/search_test_index.sbwt -q test_objects/full_pipeline/index_search/combined_input.list -b 5 -c binary
+./build/bin/sbwt_search index -o test_objects/full_pipeline/index_search/combined_output.list -i test_objects/search_test_index.sbwt -q test_objects/full_pipeline/index_search/combined_input.list -b 5 -c bool
 
 cd test_objects/full_pipeline/index_search/expected
 files=`ls *.txt`
@@ -36,6 +36,8 @@ do
     ((bad_exits+=$?))
   done
 done
+
+rm test_objects/full_pipeline/index_search/actual/*
 
 if [[ ${bad_exits} -gt 0 ]]; then
   exit 1
