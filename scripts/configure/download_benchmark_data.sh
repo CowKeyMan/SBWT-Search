cfe06e006dfdb3cbd0eba74336922ef0cdbd3b08
CowKeyMan <dancauchi1@gmail.com>
2023-03-05 13:13:19 +0200

    Change benchmark datatset

--- scripts/configure/download_benchmark_data.sh
+++ scripts/configure/download_benchmark_data.sh
@@ -1,47 +1,19 @@
 #!/bin/bash
 
-# Download the data used for benchmarking from public databases.
-# If any argument at all is passed to this script, it will instead run the 'debug' version, ie it will only download some small files, for local testing
+# Download the data used for benchmarking from public databases. The dataset
+# file list is contained `scripts/configure/benchmark_datra_list.txt`.  This
+# file contains all the files obtained from
+# https://www.ebi.ac.uk/ena/browser/view/PRJEB32631.  From the paper: Mäklin,
+# T., Thorpe, H. A., Pöntinen, A. K., Gladstone, R. A., Shao, Y., Pesonen, M.,
+# … Corander, J. (2022). Strong pathogen competition in neonatal gut
+# colonisation. Nature Communications, 13(1), 7417.
+# doi:10.1038/s41467-022-35178-5.  The original dataset is 2TB big, so I have
+# chosen to only have the first 20 files
 
-MODE=1 # 1 = the proper mode
-if [ $# -ne 0 ]; then
-	echo $#
-	MODE=0
-fi
 
 mkdir -p benchmark_objects
 cd benchmark_objects
 
-# Download Fasta
-FASTA1GB="FASTA1GB.fna"
-## Source: https://www.ncbi.nlm.nih.gov/projects/genome/guide/human/index.shtml
-if [ ${MODE} == 1 ]; then
-	wget -nc "https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh38_latest/refseq_identifiers/GRCh38_latest_genomic.fna.gz" -O "${FASTA1GB}.gz"
-else
-	## Source: https://www.ebi.ac.uk/ena/browser/view/CEFR01
-	wget -nc "ftp://ftp.ebi.ac.uk/pub/databases/ena/wgs/public/cef/CEFR01.fasta.gz" -O "${FASTA1GB}.gz"
-fi
-if test -f "${FASTA1GB}"; then
-	echo "${FASTA1GB} already unzipped, skipping "
-else
-	echo "Unzipping ${FASTA1GB}"
-	gzip -dkf "${FASTA1GB}.gz" > "${FASTA1GB}"
-fi
-
-# Download Fastq
-FASTQ1GB="FASTQ1GB.fnq"
-if [ ${MODE} == 1 ]; then
-	## Source: https://www.ebi.ac.uk/ena/browser/view/SRX11174563?show=reads
-	wget -nc "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR148/030/SRR14856430/SRR14856430_1.fastq.gz" -O "${FASTQ1GB}.gz"
-else
-	## Source: https://www.ebi.ac.uk/ena/browser/view/PRJNA602297?show=reads
-	wget -nc "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR109/049/SRR10916849/SRR10916849.fastq.gz" -O "${FASTQ1GB}.gz"
-fi
-if test -f "${FASTQ1GB}"; then
-	echo "${FASTQ1GB} already unzipped, skipping "
-else
-	echo "Unzipping ${FASTQ1GB}"
-	gzip -dkf "${FASTQ1GB}.gz" > "${FASTQ1GB}"
-fi
+wget -nc -i scripts/configure/benchmark_data_list.txt
 
 cd ..
