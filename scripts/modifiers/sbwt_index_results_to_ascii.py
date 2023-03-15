ad487046a561e6e53e800dd6949a937f2461ddc7
CowKeyMan <dancauchi1@gmail.com>
2023-03-15 02:19:11 +0200

    Add script to convert algbio/sbwt index results to my ascii format

---
+++ scripts/modifiers/sbwt_index_results_to_ascii.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+"""
+Converts output from algbio/sbwt to the ascii format produced by this program
+"""
+
+import argparse
+from contextlib import ExitStack
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-i', '--input', help='The file to convert', required=True
+)
+parser.add_argument(
+    '-o', '--output-prefix', help='Where to store the output.', required=True
+)
+args = vars(parser.parse_args())
+
+u64_bytes = 8
+byteorder = 'little'
+
+with ExitStack() as stack:
+    in_file = stack.enter_context(
+        open(args['input'], mode='r', encoding="utf-8")
+    )
+    out_file = stack.enter_context(
+        open(args['output_prefix'] + '.txt', mode='wb')
+    )
+    mode_str = 'ascii'
+    ver_str = 'v1.0'
+    out_file.write(
+        (len(mode_str)).to_bytes(u64_bytes, byteorder='little', signed=False)
+    )
+    out_file.write(mode_str.encode('ascii'))
+    out_file.write(
+        (len(ver_str)).to_bytes(u64_bytes, byteorder='little', signed=False)
+    )
+    out_file.write(ver_str.encode('ascii'))
+    for i, line in enumerate(in_file):
+        line[-1] = '\n'
+        out_file.write(line.encode('ascii'))
