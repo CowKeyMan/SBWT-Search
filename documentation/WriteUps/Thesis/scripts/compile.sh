a0a37bb4a851b16759c84f56b8cb8758ed946c04
CowKeyMan <dancauchi1@gmail.com>
2023-06-30 00:08:26 +0300

    Fix minor mistakes in thesis writeup

--- documentation/WriteUps/Thesis/scripts/compile.sh
+++ documentation/WriteUps/Thesis/scripts/compile.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
+# Compiles the thesis
+
 pdflatex -synctex=1 -interaction=nonstopmode "main.tex"
 pdflatex -synctex=1 -interaction=nonstopmode "main.tex"
 bibtex "main.aux"
+pdflatex -synctex=1 -interaction=nonstopmode "main.tex"
 pdflatex -synctex=1 -interaction=nonstopmode "main.tex"
